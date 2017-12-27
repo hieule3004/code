6621a874e9baf4e4c162ace07ee8f9373ca79200
hieule3004 <33356760+hieule3004@users.noreply.github.com>
2017-12-27 23:05:01 +0000

    Add files via upload

---
+++ DecisionTree.hs
@@ -0,0 +1,289 @@
+import Data.Maybe
+import Data.List
+
+type AttName = String
+
+type AttValue = String
+
+type Attribute = (AttName, [AttValue])
+
+type Header = [Attribute]
+
+type Row = [AttValue]
+
+type DataSet = (Header, [Row])
+
+data DecisionTree = Null |
+                    Leaf AttValue | 
+                    Node AttName [(AttValue, DecisionTree)]
+                  deriving (Eq, Show)
+
+type Partition = [(AttValue, DataSet)]
+
+type AttSelector = DataSet -> Attribute -> Attribute
+
+xlogx :: Double -> Double
+xlogx p
+  | p <= 1e-100 = 0.0
+  | otherwise   = p * log2 p 
+  where
+    log2 x = log x / log 2
+
+lookUp :: (Eq a, Show a, Show b) => a -> [(a, b)] -> b
+lookUp x table
+  = fromMaybe (error ("lookUp error - no binding for " ++ show x ++ 
+                      " in table: " ++ show table))
+              (lookup x table)
+
+--------------------------------------------------------------------
+-- PART I
+--------------------------------------------------------------------
+
+allSame :: Eq a => [a] -> Bool
+allSame xs
+  = and (zipWith (==) xs (tail xs))
+
+remove :: Eq a => a -> [(a, b)] -> [(a, b)]
+remove a xs
+  = filter ((== a) . fst) xs
+
+lookUpAtt :: AttName -> Header -> Row -> AttValue
+--Pre: The attribute name is present in the given header.
+lookUpAtt name header row
+  = head (filter (flip elem (lookUp name header)) row)
+
+removeAtt :: AttName -> Header -> Row -> Row
+removeAtt name header row
+  = filter (flip notElem (lookUp name header)) row
+
+addToMapping :: Eq a => (a, b) -> [(a, [b])] -> [(a, [b])]
+addToMapping (name, val) []
+  = [(name, [val])]
+addToMapping x@(name, val) ((name', vals) : xs)
+  | name == name' = (name, val : vals) : xs
+  | otherwise     = (name', vals) : addToMapping x xs
+
+buildFrequencyTable :: Attribute -> DataSet -> [(AttValue, Int)]
+--Pre: Each row of the data set contains an instance of the attribute
+buildFrequencyTable (_, vals) (_, rows)
+  = map freq vals
+  where
+   freq val = (val, length (filter (elem val) rows))
+
+--------------------------------------------------------------------
+-- PART II
+--------------------------------------------------------------------
+
+nodes :: DecisionTree -> Int
+nodes Null
+  = 0
+nodes (Leaf _)
+  = 1
+nodes (Node _ xs)
+  = 1 + sum (map (nodes . snd) xs)
+
+evalTree :: DecisionTree -> Header -> Row -> AttValue
+evalTree Null _ _
+  = []
+evalTree (Leaf val) _ _
+  = val
+evalTree (Node name xs) header row
+  = evalTree (lookUp (lookUpAtt name header row) xs) header row
+
+--------------------------------------------------------------------
+-- PART III
+--------------------------------------------------------------------
+
+--
+-- Given...
+-- In this simple case, the attribute selected is the first input attribute 
+-- in the header. Note that the classifier attribute may appear in any column,
+-- so we must exclude it as a candidate.
+--
+nextAtt :: AttSelector
+--Pre: The header contains at least one input attribute
+nextAtt (header, _) (classifierName, _)
+  = head (filter ((/= classifierName) . fst) header)
+
+partitionData :: DataSet -> Attribute -> Partition
+partitionData (header, rows) attr@(name, vals)
+  = map (\val -> (val, (header', rows' val))) vals
+  where 
+   header'   = filter (/= attr) header
+   rows' val = map (removeAtt name header) (filter (elem val) rows)
+
+buildTree :: DataSet -> Attribute -> AttSelector -> DecisionTree 
+buildTree (_, []) _ _
+  = Null
+buildTree dataset attr@(name, _) selector
+  = Node name' (map build (partitionData dataset attr'))
+  where
+   attr'@(name', _) = selector dataset attr
+   build (val, dataset'@(header', rows')) 
+     = (val, treeH dataset')
+     where
+      vals = map (lookUpAtt name header') rows'
+      treeH dataset'
+        | vals == []   = Null
+        | allSame vals = Leaf (head vals)
+        | otherwise    = buildTree dataset' attr selector
+
+--------------------------------------------------------------------
+-- PART IV
+--------------------------------------------------------------------
+
+entropy :: DataSet -> Attribute -> Double
+entropy (_, []) _
+  = 0.0
+entropy dataset attr
+  = - (sum (map (xlogx . (/sumAll) . fromIntegral) counts))
+  where
+   counts = snd (unzip (buildFrequencyTable attr dataset))
+   sumAll = fromIntegral (sum counts)
+
+gain :: DataSet -> Attribute -> Attribute -> Double
+gain dataset@(header, rows) attP@(_, valPs) attC
+  = entropy dataset attC - sum (map calc valPs)
+  where
+   calc valP 
+     = (sumValP / sumAttC) * entropy datasetP attC
+     where
+      datasetP  = (header, filter (elem valP) rows)
+      sumAttC   = fromIntegral (length rows)
+      sumValP   = fromIntegral (length (snd datasetP))
+
+bestGainAtt :: AttSelector
+bestGainAtt dataset@(header, _) attr@(name, _)
+  = maxAtt (map pair (filter ((/= name) . fst) header))
+  where
+   pair attr' = (attr', gain dataset attr attr')
+   maxAtt [(attr, _)] = attr
+   maxAtt (x : x' : xs)
+     | snd x < snd x' = maxAtt (x' : xs)
+     | otherwise      = maxAtt (x : xs)
+
+--------------------------------------------------------------------
+
+outlook :: Attribute
+outlook 
+  = ("outlook", ["sunny", "overcast", "rainy"])
+
+temp :: Attribute 
+temp 
+  = ("temp", ["hot", "mild", "cool"])
+
+humidity :: Attribute 
+humidity 
+  = ("humidity", ["high", "normal"])
+
+wind :: Attribute 
+wind 
+  = ("wind", ["windy", "calm"])
+
+result :: Attribute 
+result
+  = ("result", ["good", "bad"])
+
+fishingData :: DataSet
+fishingData
+  = (header, table)
+
+header :: Header
+table  :: [Row]
+header 
+  =  [outlook,    temp,   humidity, wind,    result] 
+table 
+  = [["sunny",    "hot",  "high",   "calm",  "bad" ],
+     ["sunny",    "hot",  "high",   "windy", "bad" ],
+     ["overcast", "hot",  "high",   "calm",  "good"],
+     ["rainy",    "mild", "high",   "calm",  "good"],
+     ["rainy",    "cool", "normal", "calm",  "good"],
+     ["rainy",    "cool", "normal", "windy", "bad" ],
+     ["overcast", "cool", "normal", "windy", "good"],
+     ["sunny",    "mild", "high",   "calm",  "bad" ],
+     ["sunny",    "cool", "normal", "calm",  "good"],
+     ["rainy",    "mild", "normal", "calm",  "good"],
+     ["sunny",    "mild", "normal", "windy", "good"],
+     ["overcast", "mild", "high",   "windy", "good"],
+     ["overcast", "hot",  "normal", "calm",  "good"],
+     ["rainy",    "mild", "high",   "windy", "bad" ]]
+
+--
+-- This is the same as the above table, but with the result in the second 
+-- column...
+--
+fishingData' :: DataSet
+fishingData'
+  = (header', table')
+
+header' :: Header
+table'  :: [Row]
+header' 
+  =  [outlook,    result, temp,   humidity, wind] 
+table' 
+  = [["sunny",    "bad",  "hot",  "high",   "calm"],
+     ["sunny",    "bad",  "hot",  "high",   "windy"],
+     ["overcast", "good", "hot",  "high",   "calm"],
+     ["rainy",    "good", "mild", "high",   "calm"],
+     ["rainy",    "good", "cool", "normal", "calm"],
+     ["rainy",    "bad",  "cool", "normal", "windy"],
+     ["overcast", "good", "cool", "normal", "windy"],
+     ["sunny",    "bad",  "mild", "high",   "calm"],
+     ["sunny",    "good", "cool", "normal", "calm"],
+     ["rainy",    "good", "mild", "normal", "calm"],
+     ["sunny",    "good", "mild", "normal", "windy"],
+     ["overcast", "good", "mild", "high",   "windy"],
+     ["overcast", "good", "hot",  "normal", "calm"],
+     ["rainy",    "bad",  "mild", "high",   "windy"]]
+
+fig1 :: DecisionTree
+fig1
+  = Node "outlook" 
+         [("sunny", Node "temp" 
+                         [("hot", Leaf "bad"),
+                          ("mild",Node "humidity" 
+                                       [("high",   Leaf "bad"),
+                                        ("normal", Leaf "good")]),
+                          ("cool", Leaf "good")]),
+          ("overcast", Leaf "good"),
+          ("rainy", Node "temp" 
+                         [("hot", Null),
+                          ("mild", Node "humidity" 
+                                        [("high",Node "wind" 
+                                                      [("windy",  Leaf "bad"),
+                                                       ("calm", Leaf "good")]),
+                                         ("normal", Leaf "good")]),
+                          ("cool", Node "humidity" 
+                                        [("high", Null),
+                                         ("normal", Node "wind" 
+                                                         [("windy",  Leaf "bad"),
+                                                          ("calm", Leaf "good")])])])]
+
+fig2 :: DecisionTree
+fig2
+  = Node "outlook" 
+         [("sunny", Node "humidity" 
+                         [("high", Leaf "bad"),
+                          ("normal", Leaf "good")]),
+          ("overcast", Leaf "good"),
+          ("rainy", Node "wind" 
+                         [("windy", Leaf "bad"),
+                          ("calm", Leaf "good")])]
+
+
+outlookPartition :: Partition
+outlookPartition
+  = [("sunny",   ([("temp",["hot","mild","cool"]),("humidity",["high","normal"]),
+                   ("wind",["windy","calm"]),("result",["good","bad"])],
+                  [["hot","high","calm","bad"],["hot","high","windy","bad"],
+                   ["mild","high","calm","bad"],["cool","normal","calm","good"],
+                   ["mild","normal","windy","good"]])),
+     ("overcast",([("temp",["hot","mild","cool"]),("humidity",["high","normal"]),
+                   ("wind",["windy","calm"]),("result",["good","bad"])],
+                  [["hot","high","calm","good"],["cool","normal","windy","good"],
+                   ["mild","high","windy","good"],["hot","normal","calm","good"]])),
+     ("rainy",   ([("temp",["hot","mild","cool"]),("humidity",["high","normal"]),
+                   ("wind",["windy","calm"]),("result",["good","bad"])],
+                  [["mild","high","calm","good"],["cool","normal","calm","good"],
+                   ["cool","normal","windy","bad"],["mild","normal","calm","good"],
+                   ["mild","high","windy","bad"]]))]
